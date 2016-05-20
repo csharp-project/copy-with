455ee453fcb82fcb0c5da19509b0e2ede9d8b20b
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-05-21 04:49:34 +0700

    Fixed package

--- pack.sh
+++ pack.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+rm *.nupkg
+
 xbuild /t:Build /p:Configuration=Release
 nuget pack CopyWith/CopyWith.csproj.nuspec
--- push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-nuget push $1 -Source https://www.nuget.org/api/v2/package -ApiKey $2
